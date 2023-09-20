bb5fac120ebe494a371f56a5e7cb50f8fba727d1
fargothcor <levchenya.roman@gmail.com>
2023-09-20 22:58:06 +0300

    Added comments

--- first_task/figures_area.py
+++ first_task/figures_area.py
@@ -2,26 +2,29 @@ import math
 from typing import Union
 import pytest
 
+
+# Класс для круга и метод вычисления его площади
 class Circle:
     def __init__(self, r):
         self.r = r
-
 
     def get_area(self):
         return round(math.pi*self.r ** 2, 2)
     
 
+# Класс для треугольника. Принимает длины сторон
 class Triangle:
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
 
-
+# Получение площади по формуле Герона
     def get_area(self):
         p = (self.a+self.b+self.c) / 2
         return round((p*(p-self.a)*(p-self.b)*(p-self.c)) ** (1 / 2), 2)
     
+# Проверка на прямоугольный треугольник с ограниченной точностью
     def is_rectangular(self):
         eps = 0.0001
         is_c = self.a ** 2 + self.b ** 2 - self.c ** 2 < eps
@@ -33,29 +36,30 @@ class Triangle:
         else:
             return False
 
+
+# Основной класс пакета, его следует имортировать
 class Area:
     def __init__(self, fig_type: str = '', r: Union[float, int] = None, \
-                is_rect : str = None, a: Union[float, int] = None, b: \
+                check_rect : bool = False, a: Union[float, int] = None, b: \
                 Union[float, int] = None, c: Union[float, int] = None):
         self.type = fig_type
         self.r = r
         self.a = a
         self.b = b
         self.c = c
-        self.is_rect = is_rect
+        self.check_rect = check_rect
 
-    
+    # Функция вычисления площади и проверку на прямоугольный треугольник
     def calculate_area(self):
         if self.type == 'circle':
             return Circle(self.r).get_area()
         elif self.type == 'triangle':
-            if self.is_rect == 'is_rectangular':
+            if self.check_rect:
                 return Triangle(self.a, self.b, self.c).is_rectangular()
             else:
                 return Triangle(self.a, self.b, self.c).get_area()
         else:
-            return None
-
+            raise ValueError('Unexpected figure')
 
 
 
--- first_task/test_area.py
+++ first_task/test_area.py
@@ -1,5 +1,5 @@
 import pytest
-from testtomindbox.figures_area import Area
+from first_task.figures_area import Area
 
 def test_area_higer_0():
     area = Area(fig_type = 'circle', r = -1)
@@ -20,37 +20,37 @@ def test_area_triangle():
 
 
 def test_isrec_triangle_false():
-    area = Area(fig_type = 'triangle', is_rect = 'is_rectangular', a = 6, b = 6, c = 6)
+    area = Area(fig_type = 'triangle', check_rect = True, a = 6, b = 6, c = 6)
 
     assert area.calculate_area() == False
 
 
 def test_isrec_triangle_true():
-    area = Area(fig_type = 'triangle', is_rect =  'is_rectangular', a = 3, b = 4, c = 5)
+    area = Area(fig_type = 'triangle', check_rect =  True, a = 3, b = 4, c = 5)
 
     assert area.calculate_area() == True
 
 
 def test_circle_typeerror():
     with pytest.raises(TypeError):
-            Area(fig_type ='circle', r = 'some_str').calculate_area()
+        Area(fig_type ='circle', r = 'some_str').calculate_area()
 
 
-def test_circle_typeerror():
+def test_circle_miss_r():
     with pytest.raises(TypeError):
-            Area(fig_type = 'circle').calculate_area()
+        Area(fig_type = 'circle').calculate_area()
 
 def test_triangle_typeerror():
     with pytest.raises(TypeError):
-            Area(fig_type = 'triangle', a = 1, b = 2).calculate_area()
+        Area(fig_type = 'triangle', a = 1, b = 2).calculate_area()
 
 def test_triange_typeerror():
     with pytest.raises(TypeError):
-            Area(fig_type = 'triangle', r = 'some_str', is_rect = 'another_str', a = 1, b = 1).calculate_area()
+        Area(fig_type = 'triangle', r = 'some_str', check_rect = 'another_str', a = 1, b = 1).calculate_area()
 
 
-def test_none_return():
-    area = Area(fig_type = 'not triangle and not circle', a = 1,  r = 'sometring')
+def test_raise_valueerror():
+    with pytest.raises(ValueError):
+        Area(fig_type = 'not triangle and not circle', a = 1,  r = 'some_string').calculate_area()
 
-    assert area.calculate_area() == None
 
